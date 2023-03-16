c0d438fcefe0af3e445e23a9dba7aab30fdeab4d
Julian Metzger <julianjometzger@gmail.com>
2023-03-16 14:35:18 +0100

    fixed the scoreboard overflow on smaller devices

--- app/dashboard/score-board/page.tsx
+++ app/dashboard/score-board/page.tsx
@@ -64,7 +64,7 @@ export default async function ScoreBoardPage() {
   const chapters = await getChapters();
   const users = await getUsers();
   return (
-    <center className="flex flex-col justify-between gap-4 mt-4 items-center text-center px-2">
+    <center className="flex flex-col justify-between gap-4 mt-4 items-center text-center px-2 overflow-y-auto scrollbar-none">
       <TimeTable chaptersTimes={chapters} />
       <hr />
       <LikesTable chapters={chapters} />
