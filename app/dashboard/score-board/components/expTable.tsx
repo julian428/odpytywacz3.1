ce0cda711b7ce43a872229d361d1a6b75d8e03e8
Julian Metzger <julianjometzger@gmail.com>
2023-03-20 17:07:25 +0100

    trying

--- app/dashboard/score-board/components/expTable.tsx
+++ app/dashboard/score-board/components/expTable.tsx
@@ -13,7 +13,7 @@ interface Props {
 export default function ExpTable({ users }: Props) {
   const topUsers = users;
 
-  const n = users.length;
+  const n = users?.length || 0;
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
       if (topUsers[j].exp < topUsers[j + 1].exp) {
