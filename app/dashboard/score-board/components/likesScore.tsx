2c89b929ddcce3ddaac2105d76dfac397402a29d
Julian Metzger <julianjometzger@gmail.com>
2023-03-21 21:38:28 +0100

    Fixed the like scoreboard chapter title overflow

--- app/dashboard/score-board/components/likesScore.tsx
+++ app/dashboard/score-board/components/likesScore.tsx
@@ -21,31 +21,31 @@ export default function LikesScore({ chapters }: Props) {
       </tr>
       <tr>
         <td className="w-7">1</td>
-        <td>{chapters[0]?.title || "-"}</td>
+        <td className=" max-w-[96px] truncate">{chapters[0]?.title || "-"}</td>
         <td>{chapters[0]?.owner || "-"}</td>
         <td>{chapters[0]?.likes || "-"}</td>
       </tr>
       <tr>
         <td className="w-7">2</td>
-        <td>{chapters[1]?.title || "-"}</td>
+        <td className=" max-w-[96px] truncate">{chapters[1]?.title || "-"}</td>
         <td>{chapters[1]?.owner || "-"}</td>
         <td>{chapters[1]?.likes || "-"}</td>
       </tr>
       <tr>
         <td className="w-7">3</td>
-        <td>{chapters[2]?.title || "-"}</td>
+        <td className=" max-w-[96px] truncate">{chapters[2]?.title || "-"}</td>
         <td>{chapters[2]?.owner || "-"}</td>
         <td>{chapters[2]?.likes || "-"}</td>
       </tr>
       <tr>
         <td className="w-7">4</td>
-        <td>{chapters[3]?.title || "-"}</td>
+        <td className=" max-w-[96px] truncate">{chapters[3]?.title || "-"}</td>
         <td>{chapters[3]?.owner || "-"}</td>
         <td>{chapters[3]?.likes || "-"}</td>
       </tr>
       <tr>
         <td className="w-7">5</td>
-        <td>{chapters[4]?.title || "-"}</td>
+        <td className=" max-w-[96px] truncate">{chapters[4]?.title || "-"}</td>
         <td>{chapters[4]?.owner || "-"}</td>
         <td>{chapters[4]?.likes || "-"}</td>
       </tr>
