2fd12c6fdcd299459b239b93ca975275c868dcd5
Julian Metzger <julianjometzger@gmail.com>
2023-03-21 21:17:09 +0100

    fixed the edit chapter title overflow

--- app/dashboard/my-chapters/components/EditChapter.tsx
+++ app/dashboard/my-chapters/components/EditChapter.tsx
@@ -42,7 +42,7 @@ export default function EditChapter({ chapter }: Props) {
 
   return (
     <section className="flex justify-between gap-32 items-baseline mx-2 px-2 pb-1 border-b">
-      <h2 className="capitalize text-xl">{chapter.title}</h2>
+      <h2 className="capitalize text-xl truncate w-36">{chapter.title}</h2>
       <aside className="flex gap-1">
         <StandardModal
           Label={EditIcon}
